4e5c76aad70eadaaef8adaa331369d9ab12776ac
Sahil Shaikh <shai0052@algonquinlive.com>
2021-06-24 19:15:31 -0400

    Implementations: Tube generation Tube cropping behind Tube re-centring
    Boids
    ToDo:Boid collision detection

--- Assets/Scripts/Bezier.cs
+++ Assets/Scripts/Bezier.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class Bezier : MonoBehaviour
 {
-    public List<Vector3> controlPoints;
-    public List<Vector3> points;
-
+    [HideInInspector]
+    List<Vector3> points;
+    
+    [HideInInspector]
+    List<Vector3> curvePoints;
+    
+    [HideInInspector]
     public int segments;
     
     public bool GizmosOn=true;
 
      void Awake()
      {
-        controlPoints = new List<Vector3>();
         points = new List<Vector3>();
+        curvePoints = new List<Vector3>();
+        segments = 10;
      }
 
     private void OnDrawGizmos()
@@ -24,55 +29,69 @@ public class Bezier : MonoBehaviour
             if (GizmosOn)
             {
                 
-                for (int i = 0; i < points.Count; i++)
+                for (int i = 0; i < curvePoints.Count; i++)
                 {
                     Gizmos.color = Color.grey;
-                    Gizmos.DrawSphere(points[i], 0.1f);
+                    Gizmos.DrawSphere(curvePoints[i], 0.1f);
                 }
-                for (int i = 0; i < controlPoints.Count; i++)
+                for (int i = 0; i < points.Count; i++)
                 {
                     Gizmos.color = new Color(1,0.4f,0);
-                    Gizmos.DrawSphere(controlPoints[i], 0.1f);
+                   Gizmos.DrawSphere(points[i], 0.1f);
                 }
             }
         }
     }
-
-    public Vector3[] GenerateBezeirCurve(Vector3 P1, Vector3 P2, Vector3 P3)
+    
+    public void Intialize(Vector3 P1, Vector3 P2, Vector3 P3,Vector3 P4)
     {
-        points.Clear();
-        controlPoints.Clear();
+        Reset();
 
-        int ind = 0;
+        points.Add(P1);
+        points.Add(P2);
+        points.Add(P3);
+        points.Add(P4); 
 
-        float interval = 1.0f / (segments);
-
-        for (float percent = 0; percent < 1; percent += interval, ind++)
-        {
-            points.Add( GetQuadraticPoint(P1, P2, P3, percent));
-        }
-
-        controlPoints.Add(P1);
-        controlPoints.Add(P2);
-        controlPoints.Add(P3);
-
-        return points.ToArray();
+        GenerateCurveForNewPoints();
     }
 
-    public Vector3[] GenerateBezeirCurveFromLast(Vector3 P2, Vector3 P3)
+    public void Reset()
     {
-        int ind = 0;
+        points.Clear();
+        curvePoints.Clear();
+    }
 
-        float interval = 1.0f / segments;
-        for (float percent = 0; percent < 1; percent += interval, ind++)
-        {
-            points.Add(GetQuadraticPoint(controlPoints[controlPoints.Count-1], P2, P3, percent));
-        }
+    public void AddPoint(Vector3 newPoint)
+    {
+        points.Add(points[points.Count - 1] * 2 - points[points.Count - 2]);
+        points.Add((points[points.Count - 1] + newPoint) * .5f);
+        points.Add(newPoint);
+        GenerateCurveForNewPoints();
+    }
+
+    public void RegenerateCurvePoints()
+    {
+        curvePoints.Clear();
         
-        controlPoints.Add(P2);
-        controlPoints.Add(P3);
+        float interval = 1.0f / segments;
+        
+        for (int i = 0; i < points.Count-3; i+=3)
+        {
+            for (float percent = 0; percent < 1; percent += interval)
+            {
+                curvePoints.Add(GetCubicPoint(points[i], points[i+1], points[i+2], points[i+3], percent));
+            }
+        }
+    }
 
-        return points.ToArray();
+    void GenerateCurveForNewPoints()
+    {
+        float interval = 1.0f / segments;
+
+        for (float percent = 0; percent < 1; percent += interval)
+        {
+            curvePoints.Add(GetCubicPoint(points[points.Count - 4], points[points.Count - 3], points[points.Count - 2], points[points.Count - 1], percent));
+        }
     }
 
     Vector3 GetQuadraticPoint(Vector3 P1, Vector3 P2, Vector3 P3, float percent)
@@ -80,6 +99,14 @@ public class Bezier : MonoBehaviour
         Vector3 point = GetLinearPoint(GetLinearPoint(P1, P2, percent), GetLinearPoint(P2, P3, percent), percent);
 
         return point;
+    } 
+    
+    Vector3 GetCubicPoint(Vector3 P1, Vector3 P2, Vector3 P3,Vector2 P4, float percent)
+    {
+        Vector3 curve_1 = GetLinearPoint(GetLinearPoint(P1, P2, percent), GetLinearPoint(P2, P3, percent), percent);
+        Vector3 curve_2 = GetLinearPoint(GetLinearPoint(P2, P3, percent), GetLinearPoint(P3, P4, percent), percent);
+
+        return GetLinearPoint(curve_1,curve_2,percent);
     }
 
     Vector3 GetLinearPoint(Vector3 P1, Vector3 P2, float percent)
@@ -89,5 +116,30 @@ public class Bezier : MonoBehaviour
         point = P1 + (P2 - P1) * percent;
        
         return point;
+    }
+
+    public Vector3[] GetCurvePoints()
+    {
+        return curvePoints.ToArray();
+    }
+
+    public Vector3[] GetPoints()
+    {
+        return points.ToArray();
+    }
+
+    public void RemovePoint(int index)
+    {
+        points.RemoveAt(index);
+    }
+
+    public void RemovePoints(int index,int count)
+    {
+        points.RemoveRange(index, count);
+    }
+
+    public void RemoveCurvePoints(int index, int count)
+    {
+        curvePoints.RemoveRange(index, count);
     }
 }
--- Assets/Scripts/TubeGenerator.cs
+++ Assets/Scripts/TubeGenerator.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class TubeGenerator : MonoBehaviour
 {
     [Header("Tube properties")]
-    
+
     [SerializeField]
     GameObject tube;
-    
+
     [SerializeField]
     float radius;
-    
+
     [SerializeField]
     int segments;
 
@@ -22,31 +22,80 @@ public class TubeGenerator : MonoBehaviour
     List<Vector3> tubeVerts = new List<Vector3>();
     List<Vector2> tubseVerts = new List<Vector2>();
 
-    public Vector3 P1 = new Vector3();
-    public Vector3 P2 = new Vector3();
-    public Vector3 P3 = new Vector3();
-    public Vector3 P4 = new Vector3();
-    public Vector3 P5 = new Vector3();
+    public Vector3 lastPoint = new Vector3();
+    public Vector2 perlinMapPosition;
 
     public Material material;
 
-    // Start is called before the first frame update
+    bool gameRunning = true;
+
     void Start()
     {
-        bezier = GameObject.FindGameObjectWithTag("Bezier").GetComponent<Bezier>();
-        bezier.GenerateBezeirCurve(P1, P2, P3);
+        perlinMapPosition = new Vector2(0.001f, 0.001f);
+
         bezier.GizmosOn = true;
+
+        InitializeTube();
+        tube.GetComponent<Tube>().path = bezier.GetCurvePoints();
+
+        StartCoroutine(ExtendMeshAtInterval(2));
+        StartCoroutine(CropMesh());
+        StartCoroutine(RecenterMesh());
+
+    }
+
+    void InitializeTube()
+    {
+        Vector3 P1 = transform.position;
+        Vector3 P2 = P1 + new Vector3(5, Random.Range(-0.5f, 0.5f));
+        Vector3 P3 = P2 + new Vector3(4, Random.Range(-0.5f, 0.5f));
+        Vector3 P4 = P3 + new Vector3(4, Random.Range(-0.5f, 0.5f));
+
+        bezier.Intialize(P1, P2, P3, P4);
+
+        lastPoint = P4 + Vector3.right;
+
+        for (int i = 0; i < 10; i++)
+        {
+            ExtendMesh();
+        }
+
         GenerateTubeMesh();
     }
 
-    // Update is called once per frame
-    void Update()
+    void ExtendMesh()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        Vector3 newPoint = GenerateNewPoint(lastPoint);
+
+        bezier.AddPoint(newPoint);
+
+        lastPoint = newPoint;
+
+        perlinMapPosition += new Vector2(0.01f, 0.01f);
+
+        tube.GetComponent<Tube>().path = bezier.GetCurvePoints();
+
+        GenerateTubeMesh();
+    }
+   
+    Vector3 GenerateNewPoint(Vector3 aLastPoint)
+    {
+        Vector3 newPoint;
+
+        float displacement_y = 0;
+        float displacement_z = 0;
+
+        displacement_y = Mathf.PerlinNoise(perlinMapPosition.x, perlinMapPosition.y);
+        displacement_y *= Random.Range(0, 2) >= 1 ? 1 : -1;
+
+        if (Random.Range(0, 10) > 7)
         {
-            bezier.GenerateBezeirCurveFromLast(P4, P5);
+            displacement_z = Random.Range(-1.0f, 1.0f);
         }
-       GenerateTubeMesh();
+
+        newPoint = aLastPoint + new Vector3(Random.Range(5, 8), displacement_y, 0);
+
+        return newPoint;
     }
 
     void GenerateTubeMesh()
@@ -55,20 +104,24 @@ public class TubeGenerator : MonoBehaviour
 
         Mesh mesh = new Mesh();
 
-        for (int k = 0; k < bezier.points.Count; k++)
+        int totalCurvePoints = bezier.GetCurvePoints().Length;
+        Vector3[] curve = bezier.GetCurvePoints();
+
+        for (int k = 0; k < totalCurvePoints; k++)
         {
             for (float i = 0; i < 2 * 3.14; i += (Mathf.PI * 2) / segments)
             {
-                Vector3 pos = bezier.points[k] + new Vector3(0, Mathf.Sin(i), Mathf.Cos(i))*radius;
+                Vector3 pos = curve[k] + new Vector3(0, Mathf.Sin(i), Mathf.Cos(i)) * radius;
                 tubeVerts.Add(pos);
             }
 
         }
-        int[] triangles = new int[6*(segments*(bezier.segments-1))*2];
+
+        int[] triangles = new int[6 * segments * (bezier.GetCurvePoints().Length - 1)];
 
         for (int i = 0, vertInd = 0; i < triangles.Length; i += 6, vertInd++)
         {
-            if (vertInd==0 || (vertInd+1) % segments != 0)
+            if (vertInd == 0 || (vertInd + 1) % segments != 0)
             {
                 triangles[i] = vertInd;
                 triangles[i + 1] = vertInd + segments + 1;
@@ -82,12 +135,12 @@ public class TubeGenerator : MonoBehaviour
                 triangles[i] = vertInd;
                 triangles[i + 1] = vertInd + 1;
                 triangles[i + 2] = vertInd + segments;
-                triangles[i + 3] = vertInd -(segments-1);
+                triangles[i + 3] = vertInd - (segments - 1);
                 triangles[i + 4] = triangles[i + 1];
                 triangles[i + 5] = vertInd;
             }
         }
-      
+
         mesh.vertices = tubeVerts.ToArray();
         mesh.triangles = triangles;
         mesh.RecalculateNormals();
@@ -96,6 +149,74 @@ public class TubeGenerator : MonoBehaviour
         tube.GetComponent<MeshRenderer>().material = material;
 
     }
+    
 
     
+    IEnumerator ExtendMeshAtInterval(float interval)
+    {
+        while (gameRunning)
+        {
+            yield return new WaitForSeconds(interval);
+
+            ExtendMesh();
+        }
+    }
+
+    IEnumerator CropMesh()
+    {
+        while (gameRunning)
+        {
+            yield return new WaitForSeconds(20);
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+            Vector3[] pointss = bezier.GetCurvePoints();
+            int ind = player.GetComponent<player>().index;
+            bezier.RemovePoints(0, 14);
+            bezier.RegenerateCurvePoints();
+
+            player.GetComponent<player>().index -= (15/4)*bezier.segments;
+            
+            Vector3[] pointss_1 = bezier.GetCurvePoints();
+            ind = player.GetComponent<player>().index;
+            tube.GetComponent<Tube>().path = bezier.GetCurvePoints();
+
+            GenerateTubeMesh();
+        }
+    }
+
+    IEnumerator RecenterMesh()
+    {
+        while (gameRunning)
+        {
+            yield return new WaitForSeconds(25);
+
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+            Vector3[] currentBezierPoints = bezier.GetPoints();
+
+            Vector3 PlayerDistToOrigin = -1 * (player.transform.position - player.GetComponent<player>().offsetFromCenter) - transform.position;
+
+            Vector3 DistToNextTargetLeft = player.GetComponent<player>().GetTargetPosition() - player.transform.position;
+
+            Vector3 PlayerTargetPos = player.GetComponent<player>().GetTargetPosition() + PlayerDistToOrigin;
+
+
+            for (int i = 0; i < currentBezierPoints.Length; i++)
+            {
+                currentBezierPoints[i] += PlayerDistToOrigin;
+            }
+
+            bezier.Intialize(currentBezierPoints[0], currentBezierPoints[1], currentBezierPoints[2], currentBezierPoints[3]);
+
+            for (int i = 4; i < currentBezierPoints.Length; i += 3)
+            {
+                bezier.AddPoint(currentBezierPoints[i]);
+            }
+            lastPoint = currentBezierPoints[currentBezierPoints.Length - 1];
+
+
+            player.transform.position = transform.position;
+            GenerateTubeMesh();
+        }
+    }
 }
